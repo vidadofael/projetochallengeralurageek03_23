5735cc65c7dee0c69378335b2ca9a30170118e17
vidadofael <vidadofael@gmail.com>
2023-04-03 10:30:21 -0300

    Ajuste de código js para validação de qtd itens

--- validacao/ver-tudo.js
+++ validacao/ver-tudo.js
@@ -1,8 +1,16 @@
-const verTudoBtn = document.querySelector('.produto__link--verTudo');
-const itensOcultos = document.querySelectorAll('.hidden');
+const listaProdutos = document.getElementById("lista-produtos");
+const itensProdutos = listaProdutos.querySelectorAll("[data-itemProduto]");
+const btnVerTudo = document.getElementById("verTudo");
 
-verTudoBtn.addEventListener('click', () => {
-  itensOcultos.forEach(item => {
-    item.classList.remove('hidden');
-  });
+const maxItensExibidos = 6;
+for (let i = maxItensExibidos; i < itensProdutos.length; i++) {
+  itensProdutos[i].classList.add("hidden");
+}
+
+btnVerTudo.addEventListener("click", function () {
+  // Mostra todos os itens e oculta o botão "Ver tudo"
+  for (let i = 0; i < itensProdutos.length; i++) {
+    itensProdutos[i].classList.remove("hidden");
+  }
+  btnVerTudo.style.display = "none";
 });
