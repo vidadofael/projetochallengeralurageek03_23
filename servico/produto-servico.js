b0f48914686aa959e4224cb17996d2bc45fc7569
vidadofael <vidadofael@gmail.com>
2023-04-04 11:38:39 -0300

    Criando JS para gestão de cadastro de produtos

--- servico/produto-servico.js
+++ servico/produto-servico.js
@@ -1,10 +1,14 @@
-const http = new XMLHttpRequest()
 
-http.open('GET','http://localhost:3000/produtos')
-
-http.send()
-
-http.onload = () => {
-    const data = http.response
-    console.log(data)
+/*conecta com a API (promessa de resposta)*/
+const listaProdutos = () => {
+    return fetch(`http://localhost:3000/produtos`)
+    .then(resposta => {
+        return resposta.json()
+    })
 }
+
+export const produtoService = {
+    listaProdutos
+}
+
+/*produtoService.listaProdutos*/
